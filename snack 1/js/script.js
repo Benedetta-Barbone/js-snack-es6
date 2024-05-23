b97ed787fbbc9acc0045285a0383df0eaf97e3f4
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-23 16:42:50 +0200

    aggiunto nome del tavolo e posto

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -1,7 +1,4 @@
-//SNACK 1
-//Dwayne Johnson ci ha chiesto di creare i segnaposto per il tavolo degli invitati alla sua mega festa vip.
-//Ci ha lasciato il nome del tavolo ("Tavolo Vip") e la lista degli invitati in ordine di posto:
-//[ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello']
+
 //Ma  la tipografia per stampare il tutto vuole che le mandiamo una lista di ospiti in un formato specifico, per cui dobbiamo fare in modo che ogni ospite sia un oggetto letterale javascript che ha come proprietà:
 //nome del tavolo (tableName),
 //nome dell'ospite (guestName),
@@ -10,8 +7,12 @@
 
 const listaTavolo = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
 
-const datiOspiti = listaTavolo.map(guestName => {
-  return { guestName: guestName };
+const datiOspiti = listaTavolo.map(nome => {
+  return { guestName: nome,
+           tableName: 'tavolo vip',
+           place: '',
+  };
 });
+
 
 console.log(datiOspiti)

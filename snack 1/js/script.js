8397642ab55e4449dcf10519cfc70ac5ffffd734
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-23 16:44:36 +0200

    inserito numero posto

--- snack 1/js/script.js
+++ snack 1/js/script.js
@@ -7,10 +7,10 @@
 
 const listaTavolo = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
 
-const datiOspiti = listaTavolo.map(nome => {
+const datiOspiti = listaTavolo.map((nome, index) => {
   return { guestName: nome,
            tableName: 'tavolo vip',
-           place: '',
+           place: index + 1,
   };
 });
 

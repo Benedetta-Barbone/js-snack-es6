69afcb2a2fa0a63556f1c59455a96701bd825e9a
Benedetta-Barbone <barbone1912@gmail.com>
2024-05-25 15:57:27 +0200

    creato array di oggetti snack 3

---
+++ snack 3/js/script.js
@@ -0,0 +1,11 @@
+//SNACK 3
+//Creare un array di oggetti:
+//Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+//Stampare in console la bici con peso minore utilizzando destructuring e template literal
+
+const biciclette = [
+  { nome: 'Bicicletta1', peso: 8.2 },
+  { nome: 'Bicicletta2', peso: 7.9 },
+  { nome: 'Bicicletta3', peso: 6.8 },
+  { nome: 'Bicicletta4', peso: 8.4 }
+];
